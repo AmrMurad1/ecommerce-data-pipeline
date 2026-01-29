34f4d2e57ef02cf7c8ec0c0c249ef01be281b952
Amr Murad <amrmurad2003@gmail.com>
2026-01-29 15:52:33 +0200

    ingestion dag added

---
+++ dags/ingest_only_dag.py
@@ -0,0 +1,28 @@
+
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.providers.standard.operators.bash import BashOperator
+
+# Default arguments for the DAG
+default_args = {
+    'owner': 'airflow',
+    'retries': 2,
+    'retry_delay': timedelta(minutes=5),
+    'start_date': datetime(2025, 1, 1),
+}
+
+# Define the DAG
+with DAG(
+    dag_id='ingest_dag',
+    description='Simple DAG that runs the raw data ingestion script',
+    default_args=default_args,
+    schedule=None,  # Manual trigger only
+    catchup=False,
+    tags=['ingest', 'bronze'],
+) as dag:
+    
+    # Task: Run the ingestion script
+    ingest_task = BashOperator(
+        task_id='run_ingestion_script',
+        bash_command='python /opt/airflow/scripts/ingest_raw.py',
+    )
--- scripts/ingest_raw.py
+++ scripts/ingest_raw.py
@@ -3,14 +3,12 @@ from sqlalchemy import create_engine, text
 import os
 import sys
 
-DB_USER = os.getenv("DB_USER", "airflow_user")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST", "postgres")
-DB_PORT = os.getenv("DB_PORT", "5432")
-DB_NAME = os.getenv("DB_NAME", "ecommerce")
-
-if not DB_PASSWORD:
-    raise ValueError("DB_PASSWORD environment variable is required!")
+# Database credentials
+DB_USER = "airflow_user"
+DB_PASSWORD = "amoory2003"
+DB_HOST = "postgres"
+DB_PORT = "5432"
+DB_NAME = "ecommerce"
 
 connection_string = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
